003377229468bc1401ee62b5aac8c5725c4ebac4
m09u5 <ryussexd@gmail.com>
2024-05-09 09:57:32 +0200

    logs100

--- lab4.sh
+++ lab4.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
 if [ "$1" = "--date" ]; then
     current_date=$(date +"%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
+elif [ "$1" = "--logs" ]; then
+    for ((i=1; i<=100; i++)); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Utworzony przez: $0" >> "$filename"
+        echo "Data utworzenia: $(date)" >> "$filename"
+    done
 fi
