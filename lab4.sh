07a7c31b4a11c0483856bdd02829f9a8e823576d
m09u5 <ryussexd@gmail.com>
2024-05-09 10:16:36 +0200

    cz2

--- lab4.sh
+++ lab4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$1" = "--date" ]; then
+if [[ "$1" = "--date" || "$1" = "-d" ]]; then
     current_date=$(date +"%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
-elif [ "$1" = "--logs" ]; then
+elif [[ "$1" = "--logs" || "$1" = "-l" ]]; then
     num_logs=100
     
     if [ -n "$2" ]; then
@@ -16,11 +16,30 @@ elif [ "$1" = "--logs" ]; then
         echo "Utworzony przez: $0" >> "$filename"
         echo "Data utworzenia: $(date)" >> "$filename"
     done
-elif [ "$1" = "--help" ]; then
+elif [[ "$1" = "--help" || "$1" = "-h" ]]; then
     echo "Dostêpne opcje:"
-    echo "--date : Wyœwietla dzisiejsz¹ datê"
-    echo "--logs [liczba_plików] : Tworzy automatycznie podan¹ liczbê plików logx.txt"
-    echo "--help : Wyœwietla wszystkie dostêpne opcje"
+    echo "--date (-d) : Wyœwietla dzisiejsz¹ datê"
+    echo "--logs [liczba_plików] (-l) : Tworzy automatycznie podan¹ liczbê plików logx.txt"
+    echo "--init : Klonuje ca³e repozytorium do katalogu w którym zosta³ uruchomiony oraz ustawia œcie¿kê w zmiennej œrodowiskowej PATH"
+    echo "--error [liczba_plików] (-e) : Tworzy podan¹ liczbê plików errorx/errorx.txt. Domyœlnie tworzy 100 plików"
+    echo "--help (-h) : Wyœwietla wszystkie dostêpne opcje"
+elif [[ "$1" = "--init" ]]; then
+    git clone git@github.com:m09u5/lab4.git ./lab4
+    export PATH=$PATH:./lab4
+elif [[ "$1" = "--error" || "$1" = "-e" ]]; then
+    num_errors=100
+    
+    if [ -n "$2" ]; then
+        num_errors=$2
+    fi
+    
+    for ((i=1; i<=$num_errors; i++)); do
+        errorfile="error${i}.txt"
+        mkdir -p errorx
+        echo "B³¹d numer $i" > "errorx/$errorfile"
+        echo "Utworzony przez: $0" >> "errorx/$errorfile"
+        echo "Data utworzenia: $(date)" >> "errorx/$errorfile"
+    done
 else
-    echo "Nieznana opcja. U¿yj '--help' aby wyœwietliæ dostêpne opcje."
+    echo "Nieznana opcja. U¿yj '-h' aby wyœwietliæ dostêpne opcje."
 fi
